6b699e4f4bdad0e34d5e7bb9e3d2ee14fcfd340f
thehouseplant <sean.collins@outlook.com>
2022-05-07 16:05:22 -0700

    Add public subnets in each availability zone

--- main.tf
+++ main.tf
@@ -17,6 +17,47 @@ provider "aws" {
 
 # Networking
 # VPC
-resources "aws_vpc" "vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+# Public subnets
+resource "aws_subnet" "public_a" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Public Subnet A"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "Public Subnet B"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-west-2c"
+
+  tags = {
+    Name = "Public Subnet C"
+  }
+}
+
+resource "aws_subnet" "public_d" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-west-2d"
+
+  tags = {
+    Name = "Public Subnet D"
+  }
 }
