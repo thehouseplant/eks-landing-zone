2417ef3240f22c289bf8d48c6aad078a6f1af648
thehouseplant <sean.collins@outlook.com>
2022-05-07 16:50:55 -0700

    Add DNS and tags to VPC configuration

--- main.tf
+++ main.tf
@@ -23,7 +23,13 @@ provider "aws" {
 # Networking resources
 # VPC
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "ECS Cluster VPC"
+  }
 }
 
 # Internet gateway
