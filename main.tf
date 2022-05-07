f3408ccb42be3c9e7d1ae8732b98a015fd128635
thehouseplant <sean.collins@outlook.com>
2022-05-07 16:30:14 -0700

    Add basic ECS cluster with logging and encryption

--- main.tf
+++ main.tf
@@ -335,3 +335,39 @@ resource "aws_nat_gateway" "private_d" {
     Name = "Private NAT D"
   }
 }
+
+
+
+# ECS
+# Cluster resources
+resource "aws_ecs_cluster" "cluster" {
+  name = "ECS-CLUSTER"
+
+  setting {
+    name = "Cluster"
+    value = "enabled"
+  }
+
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.cluster_key.arn
+      logging = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name = aws_cloudwatch_log_group.cluster_logs.name
+      }
+    }
+  }
+}
+
+# Logging resources
+resource "aws_cloudwatch_log_group" "cluster_logs" {
+  name = "ECS-CLUSTER-LOGS"
+}
+
+# KMS resources
+resource "aws_kms_key" "cluster_key" {
+  description = "ClusterKey"
+  deletion_window_in_days = 7
+}
