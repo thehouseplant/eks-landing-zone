fa093f4b5fc6f110d043c0ab11b119170c9ffb6f
thehouseplant <sean.collins@outlook.com>
2022-05-07 16:34:54 -0700

    Add ECS service and task definition

--- main.tf
+++ main.tf
@@ -344,18 +344,18 @@ resource "aws_ecs_cluster" "cluster" {
   name = "ECS-CLUSTER"
 
   setting {
-    name = "Cluster"
+    name  = "Cluster"
     value = "enabled"
   }
 
   configuration {
     execute_command_configuration {
       kms_key_id = aws_kms_key.cluster_key.arn
-      logging = "OVERRIDE"
+      logging    = "OVERRIDE"
 
       log_configuration {
         cloud_watch_encryption_enabled = true
-        cloud_watch_log_group_name = aws_cloudwatch_log_group.cluster_logs.name
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.cluster_logs.name
       }
     }
   }
@@ -368,6 +368,60 @@ resource "aws_cloudwatch_log_group" "cluster_logs" {
 
 # KMS resources
 resource "aws_kms_key" "cluster_key" {
-  description = "ClusterKey"
+  description             = "ClusterKey"
   deletion_window_in_days = 7
+}
+
+# Service A
+resource "aws_ecs_task_definition" "service_a" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "service-a"
+      image     = "service-a"
+      cpu       = 1
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b, us-west-2c, us-west-2d]"
+  }
+}
+
+resource "aws_ecs_service" "service_a" {
+  name            = "ECS-SERVICE-A"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.service_a.arn
+  desired_count   = 4
+  iam_role        = ""
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = ""
+    container_name   = "service-a"
+    container_port   = 80
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b, us-west-2c, us-west-2d]"
+  }
 }
