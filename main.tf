0627d2e23676b2d44f013645e3ba2bacaecf3d1f
thehouseplant <sean.collins@outlook.com>
2022-05-07 16:24:07 -0700

    Add private route tables and associations

--- main.tf
+++ main.tf
@@ -238,6 +238,67 @@ resource "aws_subnet" "private_d" {
   }
 }
 
+# Private subnet route tables and associations
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {}
+
+  tags = {
+    Name = "Private Route Table A"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {}
+
+  tags = {
+    Name = "Private Route Table B"
+  }
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route_table" "private_c" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {}
+
+  tags = {
+    Name = "Private Route Table C"
+  }
+}
+
+resource "aws_route_table_association" "private_c" {
+  subnet_id      = aws_subnet.private_c.id
+  route_table_id = aws_route_table.private_c.id
+}
+
+resource "aws_route_table" "private_d" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {}
+
+  tags = {
+    Name = "Private Route Table D"
+  }
+}
+
+resource "aws_route_table_association" "private_d" {
+  subnet_id      = aws_subnet.private_d.id
+  route_table_id = aws_route_table.private_d.id
+}
+
 # Private NAT gateways
 resource "aws_nat_gateway" "private_a" {
   connectivity_type = "private"
