155ec8c1fc9661601a0d5778bd363ff1b09b1bf3
thehouseplant <sean.collins@outlook.com>
2022-06-03 13:34:54 -0700

    Hard-code AZ references to fix an error

--- vpc.tf
+++ vpc.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_availability_zones" "available" {}
-
 locals {
   cluster_name = "EKS-CLUSTER-TEST"
 }
@@ -14,7 +12,7 @@ module "vpc" {
 
   name                 = "EKS-CLUSTER-VPC"
   cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+  azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
