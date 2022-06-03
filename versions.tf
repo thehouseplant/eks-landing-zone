d7a5e2d81221578de72ef8fb051d8808b615e585
thehouseplant <sean.collins@outlook.com>
2022-06-03 14:23:19 -0700

    Remove unnecessary provider

--- versions.tf
+++ versions.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.20.0"
     }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "2.1.0"
-    }
   }
 
   required_version = ">= 0.14"
