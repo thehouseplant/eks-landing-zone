4a008105892531302a6181c2ec6e6d75a4c0a812
thehouseplant <sean.collins@outlook.com>
2022-06-03 13:23:17 -0700

    Terraformatting

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "cluster_id" {
   description = "EKS cluster ID"
-  value = module.eks.cluster_id
+  value       = module.eks.cluster_id
 }
 
 output "cluster_endpoint" {
   description = "EKS control plane endpoint"
-  value = module.eks.cluster_endpoint
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
   description = "Security group IDs attached to the EKS cluster"
-  value = module.eks.cluster_security_group_id
+  value       = module.eks.cluster_security_group_id
 }
 
 module "kubectl_config" {
   description = "kubectl configuration"
-  value = module.eks.kubeconfig
+  value       = module.eks.kubeconfig
 }
 
 output "config_map_aws_auth" {
   description = "Kubernetes configuration to authenticate to the EKS cluster"
-  value = module.eks.config_map_aws_auth
+  value       = module.eks.config_map_aws_auth
 }
