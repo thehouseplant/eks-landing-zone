4b56a46095bc180f30920625afc75ebdf37412af
thehouseplant <sean.collins@outlook.com>
2022-06-03 14:20:52 -0700

    Add EKS worker node role

--- iam.tf
+++ iam.tf
@@ -1,5 +1,7 @@
+# EKS cluser role
 resource "aws_iam_role" "cluster_role" {
   name               = "eksclusterrole"
+
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -19,4 +21,41 @@ POLICY
 resource "aws_iam_role_policy_attachment" "cluster_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster_role.name
+}
+
+
+
+# EKS worker nodes role
+resource "aws_iam_role" "nodes_role" {
+  name = "eksnodesrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes_role.name
 }
