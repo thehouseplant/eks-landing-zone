087b074f242296c12b2dc9be950e1190b87d81b0
thehouseplant <sean.collins@outlook.com>
2022-06-03 13:32:38 -0700

    Update database name references

--- database.tf
+++ database.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "postgres" {
-  identifier                = "EKS-CLUSTER-RDS"
+  identifier                = "eks-test"
   allocated_storage         = 5
   backup_retention_period   = 2
   backup_window             = "01:00-01:30"
@@ -8,13 +8,13 @@ resource "aws_db_instance" "postgres" {
   engine                    = "postgres"
   engine_version            = "13.7"
   instance_class            = "db.t3.small"
-  name                      = "EKS-CLUSTER-RDS"
+  db_name                   = "eks-test"
   username                  = "administrator"
   password                  = ""
   port                      = "5432"
   db_subnet_group_name      = ""
   vpc_security_group_ids    = [aws_security_group.rds_sg.id]
   skip_final_snapshot       = true
-  final_snapshot_identifier = "EKS-CLUSTER-RDS-FINAL"
+  final_snapshot_identifier = "eks-test-final"
   publicly_accessible       = false
 }
