fdf4059998abad509f0fd1b557db65c9f90624f4
thehouseplant <sean.collins@outlook.com>
2022-06-04 14:14:10 -0700

    Add mutability and scanning configuration

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,8 @@
 resource "aws_ecr_repository" "repo" {
-  name = "${var.prefix}-REPO"
+  name                 = "${var.prefix}-REPO"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
