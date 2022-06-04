4df4b4e07157e8b0ce7d6f7ee7b94f28d3b73f4a
thehouseplant <sean.collins@outlook.com>
2022-06-04 16:38:56 -0700

    Enable slow logging for ElastiCache

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,3 +1,7 @@
-resource "aws_cloudwatch_log_group" "cluster_logs" {
-  name = "${var.prefix}-LOGS"
+resource "aws_cloudwatch_log_group" "eks_logs" {
+  name = "${var.prefix}-EKS"
+}
+
+resource "aws_cloudwatch_log_group" "elasticache_logs" {
+  name = "${var.prefix}-ELASTICACHE"
 }
--- elasticache.tf
+++ elasticache.tf
@@ -6,4 +6,11 @@ resource "aws_elasticache_cluster" "cluster" {
   parameter_group_name = var.elasticache_parameter_group
   engine_version       = var.elasticache_version
   port                 = var.elasticache_port
+
+  log_delivery_configuration {
+    destination      = aws_cloudwatch_log_group.elasticache_logs.name
+    destination_type = "cloudwatch-logs"
+    log_format       = "text"
+    log_type         = "slow-log"
+  }
 }
