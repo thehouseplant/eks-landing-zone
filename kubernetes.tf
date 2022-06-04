675b86d98a9a497f0ab7dc0710e8e9707edc2d65
thehouseplant <sean.collins@outlook.com>
2022-06-04 15:10:21 -0700

    Finish Kubernetes ingress deployment

--- kubernetes.tf
+++ kubernetes.tf
@@ -110,3 +110,87 @@ resource "kubernetes_deployment" "alb_ingress" {
     }
   }
 }
+
+resource "kubernetes_ingress" "main" {
+  metadata {
+    name = "main-ingress"
+    annotations = {
+      "alb.ingress.kubernetes.io/scheme"               = "internet-facing"
+      "kubernetes.io/ingress.class"                    = "alb"
+      "alb.ingress.kubernetes.io/subnets"              = "${aws_subnet.public_a.id}, ${aws_subnet.public_b.id}, ${aws_subnet.public_c.id}"
+      "alb.ingress.kubernetes.io/certificate-arn"      = "${aws_acm_certificate.cert.arn}"
+      "alb.ingress.kubernetes.io/listen-ports"         = <<JSON
+[
+  {"HTTP": 80},
+  {"HTTPS": 443}
+]
+JSON
+      "alb.ingress.kubernetes.io/actions.ssl-redirect" = <<JSON
+{
+  "Type": "redirect",
+  "RedirectConfig": {
+    "Protocol": "HTTPS",
+    "Port": "443",
+    "StatusCode": "HTTP_301"
+  }
+}
+JSON
+    }
+  }
+
+  spec {
+    rule {
+      host = "app.example.com"
+      http {
+        path {
+          backend {
+            service_name = "ssl-redirect"
+            service_port = "use-annotation"
+          }
+          path = "/*"
+        }
+        path {
+          backend {
+            service_name = "app-service1"
+            service_port = 80
+          }
+          path = "/service1"
+        }
+        path {
+          backend {
+            service_name = "app-service2"
+            service_port = 80
+          }
+          path = "/service2"
+        }
+      }
+    }
+
+    rule {
+      host = "api.example.com"
+      http {
+        path {
+          backend {
+            service_name = "ssl-redirect"
+            service_port = "use-annotation"
+          }
+          path = "/*"
+        }
+        path {
+          backend {
+            service_name = "api-service1"
+            service_port = 80
+          }
+          path = "/service3"
+        }
+        path {
+          backend {
+            service_name = "api-service2"
+            service_port = 80
+          }
+          path = "/graphq4"
+        }
+      }
+    }
+  }
+}
