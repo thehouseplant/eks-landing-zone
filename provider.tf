8f226a099ddcd74fa7b30135c27f64114d508ab7
thehouseplant <sean.collins@outlook.com>
2022-06-04 16:59:08 -0700

    Migrate default tags to provider configuration

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      Description = "EKS Landing Zone"
+      Environment = "POC"
+      Contact     = "user@example.com"
+    }
+  }
 }
