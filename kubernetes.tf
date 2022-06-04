ef1f99d648e8c95545edb682283e5187e818e77a
thehouseplant <sean.collins@outlook.com>
2022-06-04 14:48:27 -0700

    Add ALB ingress controller and cluster binding

--- kubernetes.tf
+++ kubernetes.tf
@@ -8,3 +8,45 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.main.token
   load_config_file       = false
 }
+
+resource "kubernetes_cluster_role" "alb_ingress" {
+  metadata {
+    name = "${var.prefix}-INGRESS-CONTROLLER"
+    labels = {
+      "app.kubernetes.io/name" = lower("${var.prefix}-ingress-controller")
+    }
+  }
+
+  rule {
+    api_groups = ["", "extensions"]
+    resources  = ["configmaps", "endpoints", "events", "ingresses", "ingresses/status", "services"]
+    verbs      = ["create", "get", "list", "patch", "update", "watch"]
+  }
+
+  rule {
+    api_groups = ["", "extensions"]
+    resources  = ["namespaces", "nodes", "pods", "secrets", "services"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "alb_ingress" {
+  metadata {
+    name = "${var.prefix}-INGRESS-CONTROLLER"
+    labels = {
+      "app.kubernetes.io/name" = lower("${var.prefix}-ingress-controller")
+    }
+  }
+
+  rule {
+    api_group = "rbac.authorized.k8s.io"
+    kind      = "ClusterRole"
+    name      = lower("${var.prefix}-ingress-controller")
+  }
+
+  rule {
+    kind      = "ServiceAccount"
+    name      = lower("${var.prefix}-ingress-controller")
+    namespace = "kube-system"
+  }
+}
