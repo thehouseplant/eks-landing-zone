28cb4cc9184ac7a16b0c889b848c1a4fcdecbf5e
thehouseplant <sean.collins@outlook.com>
2022-06-04 17:25:51 -0700

    Attach load balancer security group

--- lb.tf
+++ lb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "ingress" {
   name               = "${var.prefix}-ALB"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = []
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.public_c.id]
 
   enable_deletion_protection = true
