ed554d661ba36a52fd0f6d5beb3cb90bc9612e0f
thehouseplant <sean.collins@outlook.com>
2022-06-05 12:02:48 -0700

    Lock down default S3 bucket

--- s3.tf
+++ s3.tf
@@ -1,6 +1,14 @@
 resource "aws_s3_bucket" "s3_logs" {
   bucket = lower("${var.prefix}-s3-bucket-logs")
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        ssl_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
     Name = "EKS Logging Bucket"
   }
@@ -9,4 +17,13 @@ resource "aws_s3_bucket" "s3_logs" {
 resource "aws_s3_bucket_acl" "s3_acl" {
   bucket = aws_s3_bucket.s3_logs.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.s3_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
