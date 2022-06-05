26cc67bc096efe86e4d76ebfc5ff4f33eec54129
thehouseplant <sean.collins@outlook.com>
2022-06-04 17:00:50 -0700

    Add CloudWatch Name tags

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,8 +1,16 @@
 resource "aws_cloudwatch_log_group" "eks_logs" {
   name              = lower("/aws/eks/eks-lz-cluster/cluster")
   retention_in_days = 7
+
+  tags = {
+    Name = "EKS Landing Zone Cluster Logs"
+  }
 }
 
 resource "aws_cloudwatch_log_group" "elasticache_logs" {
   name = "${var.prefix}-ELASTICACHE"
+
+  tags = {
+    Name = "EKS Landing Zone ElastiCache Logs"
+  }
 }
