6f76372c4a61385f5022e252fb4f0bca41fa3753
thehouseplant <sean.collins@outlook.com>
2022-06-05 12:42:25 -0700

    Convert standalone RDS instance to Aurora cluster

--- rds.tf
+++ rds.tf
@@ -1,20 +1,21 @@
-resource "aws_db_instance" "postgres" {
-  identifier                = lower("${var.prefix}-db")
-  allocated_storage         = 5
-  backup_retention_period   = 2
-  backup_window             = "01:00-01:30"
-  maintenance_window        = "sun:03:00-sun:03:30"
-  multi_az                  = true
-  engine                    = var.aurora_engine
-  engine_version            = var.aurora_version
-  instance_class            = var.aurora_size
-  db_name                   = lower("${var.prefix}-db")
-  username                  = var.aurora_user
-  password                  = ""
-  port                      = "5432"
-  db_subnet_group_name      = ""
-  vpc_security_group_ids    = [aws_security_group.rds_sg.id]
-  skip_final_snapshot       = true
-  final_snapshot_identifier = lower("${var.prefix}-final")
-  publicly_accessible       = false
+resource "aws_rds_cluster" "cluster" {
+  cluster_identifier           = "${var.prefix}-RDS-CLUSTER"
+  engine                       = var.aurora_engine
+  engine_version               = var.rds_version
+  availability_zones           = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  database_name                = var.rds_name
+  master_username              = var.rds_user
+  master_password              = var.rds_pass
+  backup_retention_period      = 5
+  preferred_backup_window      = "01:00-01:30"
+  preferred_maintenance_window = "sun:03:00-sun:03:30"
+}
+
+resource "aws_rds_cluster_instance" "instance" {
+  count              = var.rds_count
+  identifier         = lower("${var.prefix}-rds-instance-${count.index}")
+  cluster_identifier = aws_rds_cluster.cluster.id
+  instance_class     = var.rds_size
+  engine             = aws_rds_cluster.cluster.engine
+  engine_version     = aws_rds_cluster.cluster.engine_version
 }
